4bd38d77e04b4066b4432c06b7703e32d3edf43e
Tim Diggins <tim@red56.uk>
2021-03-05 16:02:34 +0000

    add default display for pupil and school

--- app/models/pupil.rb
+++ app/models/pupil.rb
@@ -2,7 +2,12 @@ class Pupil < ApplicationRecord
   belongs_to :school, inverse_of: :pupils
   has_many :pupil_measurements, inverse_of: :pupil
 
+  alias_attribute :to_s, :full_name
   before_create do
     self.external_ident ||= SecureRandom.hex(30)
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 end
